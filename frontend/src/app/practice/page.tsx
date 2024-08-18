8d214adc2f6870a1a73c0307aa63372630d7c66a
Aditya Rao <adityarao2005@gmail.com>
2024-08-17 20:37:44 -0400

    added basic intro of practice page

---
+++ frontend/src/app/practice/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+import styles from "@/app/practice/page.module.css";
+import uploadImage from "@/assets/upload button.png"
+
+export default function Component() {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    return (
+        <div className={styles.uploadbackground}>
+            <div className={styles.uploadbackgroundcentering}>
+                <div className={styles.title}><h1>paste study material to get started</h1></div>
+                <div className={styles.uploadbox}>
+                    Prompt with lecture notes, study material, etc (instruction message with prompt ideas)....
+                    <div className={styles.buttonContainer}>
+                        <button className={styles.uploadbutton}>
+                            <img src={uploadImage.src} alt="My Image" className={styles.image} />{" "} Upload
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
